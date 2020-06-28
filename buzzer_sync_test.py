6e83f7fa0894a3ce62ed0c92843c2ddea0a8770c
NiallBunting <niall.bunting@bjss.com>
2020-06-28 22:42:39 +0100

    Add buzzer and sync testing file
    This activates to make sure both are working.

---
+++ buzzer_sync_test.py
@@ -0,0 +1,31 @@
+#Libraries
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Disable warnings (optional)
+GPIO.setwarnings(False)
+#Select GPIO mode
+GPIO.setmode(GPIO.BCM)
+#Set buzzer - pin 23 as output
+sync=23
+buzzer=24
+GPIO.setup(buzzer,GPIO.OUT)
+GPIO.setup(sync,GPIO.OUT)
+
+# Press sync - will need to connect here
+GPIO.output(sync,GPIO.HIGH)
+print ("Start Sync")
+sleep(0.5) # Delay in seconds
+GPIO.output(sync,GPIO.LOW)
+
+#Run forever loop
+for x in range(1,5):
+    GPIO.output(buzzer,GPIO.HIGH)
+    print ("Beep")
+    sleep(0.5) # Delay in seconds
+    GPIO.output(buzzer,GPIO.LOW)
+    print ("No Beep")
+    sleep(0.5)
+
+
+GPIO.cleanup()
